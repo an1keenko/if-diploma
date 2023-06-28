6e1ffdd64c88028ebf83c2fffb28b0063c771f83
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-06-28 23:59:43 +0200

    #3 added icons and a category section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
-import { Header } from "./components/Header.tsx";
-import { Container } from "./components/Container.tsx";
-
+import { Header } from './components/Header.tsx'
+import { Container } from './components/Container.tsx'
+import { CategorySection } from './components/CategorySection.tsx'
 
 function App() {
-
   return (
     <>
       <Container>
         <Header />
+        <CategorySection />
       </Container>
     </>
   )
---
+++ src/components/CategorySection.tsx
@@ -0,0 +1,20 @@
+import './CategorySection.css'
+
+export const CategorySection = () => {
+  return (
+    <div className="category">
+      <h2>
+        Shop by the <b>Category</b>
+      </h2>
+      <div className="category__buttons">
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  )
+}
